151e290344333f44654d4407bf6b03439fbda801
jungelCui <835554405@qq.com>
2017-07-28 17:31:02 +0800

    jungle fir 5:30 搜索，分类播放

---
+++ app/components/routers/detail.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<xheader></xheader>
+		<xdetail></xdetail>
+	</div>
+</template>
+<script>
+import xheader from "../xheader.vue"
+import xdetail from "../xdetail.vue"
+	export default{
+		components:{
+			xheader,
+			xdetail
+		},
+		mounted(){
+			
+		}
+	}
+</script>
